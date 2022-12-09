6654a4f126715fae7b609fbd3ed0aa74f23b911c
doonitius <dkitsakulpaisan@gmail.com>
2022-12-09 21:56:43 +0700

    initial project and gui

---
+++ main.py
@@ -0,0 +1,195 @@
+from tkinter import *
+from project_menu import add_project_window, edit_project_window, delete_project_window
+
+def close_main(app):
+    app.destroy()
+
+def back_to_main(app):
+    app.destroy()
+    main()
+
+def director_menu(app):
+    close_main(app)
+    print('You are director')
+    app_director = Tk()
+    app_director.title("Task-Scheduler @Director")
+
+    Label(app_director,text="Director", font=20).pack()
+
+    menubar = Menu(app_director)
+    app_director.config(menu=menubar)
+
+    project_menu = Menu(
+        menubar,
+        tearoff=0
+    )
+
+    exit_menu = Menu(
+        menubar,
+        tearoff=0
+    )
+
+    # add menu items to the File menu
+    project_menu.add_command(label='Add Project', command=add_project_window)
+    project_menu.add_command(label='Edit Project', command=edit_project_window)
+    project_menu.add_command(label='Delete Project', command=delete_project_window)
+
+    menubar.add_cascade(
+        label="Project",
+        menu=project_menu
+    )
+
+    exit_menu.add_command(
+        label='Return to Root User',
+        command=lambda: back_to_main(app_director)
+    )
+    # add Exit menu item
+    exit_menu.add_command(
+        label='Exit Program',
+        command=app_director.destroy
+    )
+
+    menubar.add_cascade(
+        label="Exit",
+        menu=exit_menu
+    )
+
+    app_director.geometry("500x500")
+    app_director.mainloop()
+
+
+def manager_menu(app):
+    close_main(app)
+    print('You are manager')
+    app_manager = Tk()
+    app_manager.title("Task-Scheduler @Manager")
+
+    Label(app_manager, text="Manager", font=20).pack()
+
+    menubar = Menu(app_manager)
+    app_manager.config(menu=menubar)
+
+    task_menu = Menu(
+        menubar,
+        tearoff=0
+    )
+
+    team_menu = Menu(
+        menubar,
+        tearoff=0
+    )
+
+    exit_menu = Menu(
+        menubar,
+        tearoff=0
+    )
+
+    # add menu items to the File menu
+    task_menu.add_command(label='Add Task')
+    task_menu.add_command(label='Edit Task')
+    task_menu.add_command(label='Delete Task')
+
+    menubar.add_cascade(
+        label="Task",
+        menu=task_menu
+    )
+
+    team_menu.add_command(label='Add Team')
+    team_menu.add_command(label='Edit Team')
+    team_menu.add_command(label='Delete Team')
+
+    menubar.add_cascade(
+        label="Team",
+        menu=team_menu
+    )
+
+    exit_menu.add_command(
+        label='Return to Root User',
+        command=lambda: back_to_main(app_manager)
+    )
+    # add Exit menu item
+    exit_menu.add_command(
+        label='Exit Program',
+        command=app_manager.destroy
+    )
+
+    menubar.add_cascade(
+        label="Exit",
+        menu=exit_menu
+    )
+
+
+    app_manager.geometry("500x500")
+    app_manager.mainloop()
+
+
+def developer_menu(app):
+    close_main(app)
+    print('You are developer')
+    app_developer = Tk()
+    app_developer.title("Task-Scheduler @Developer")
+
+    Label(app_developer, text="Developer", font=20).pack()
+    menubar = Menu(app_developer)
+    app_developer.config(menu=menubar)
+
+    task_menu = Menu(
+        menubar,
+        tearoff=0
+    )
+
+    exit_menu = Menu(
+        menubar,
+        tearoff=0
+    )
+
+    # add menu items to the File menu
+    task_menu.add_command(label='View Task Board')
+    task_menu.add_command(label='Update Task Status')
+    task_menu.add_command(label='Review Request')
+
+    menubar.add_cascade(
+        label="Task",
+        menu=task_menu
+    )
+
+    exit_menu.add_command(
+        label='Return to Root User',
+        command=lambda: back_to_main(app_developer)
+    )
+    # add Exit menu item
+    exit_menu.add_command(
+        label='Exit Program',
+        command=app_developer.destroy
+    )
+
+    menubar.add_cascade(
+        label="Exit",
+        menu=exit_menu
+    )
+
+    app_developer.geometry("500x500")
+    app_developer.mainloop()
+
+
+
+def main():
+    app = Tk()
+    app.title('Task-Scheduler')
+
+    select_user = Label(text="Who are you?",font=20)
+    select_user.pack()
+
+    director_button = Button(app,text='Director', command= lambda: director_menu(app))
+    manager_button = Button(app, text='Manager', command= lambda: manager_menu(app))
+    developer_button = Button(app,text='Developer', command= lambda: developer_menu(app))
+
+    director_button.pack(side = LEFT, expand =True)
+    manager_button.pack(side=LEFT, expand=True)
+    developer_button.pack(side=LEFT, expand=True)
+
+    app.geometry("500x500")
+    app.mainloop()
+    
+if __name__ == "__main__":
+    main()
---
+++ project_menu.py
@@ -0,0 +1,124 @@
+from tkinter import *
+
+
+def submit_add_project(app_add_project, name, desc, start_dt, task):
+    app_add_project.destroy()
+    print("Project Added")
+    print(name, desc, start_dt, task)
+
+
+def submit_edit_project(app_add_project, name, desc, start_dt, task):
+    app_add_project.destroy()
+    print("Project Edited")
+    print(name, desc, start_dt, task)
+
+
+def cancel_button_project(app_add_project):
+    app_add_project.destroy()
+
+
+def add_project_window():
+    app_add_project = Tk()
+    app_add_project.title("Add New Project")
+
+    # Label(app_add_project, text="Add New Project Here", font=20).pack()
+
+    Label(app_add_project, text="Project Name").grid(row=0)
+    Label(app_add_project, text="Project Description").grid(row=1)
+    Label(app_add_project, text="Start Date").grid(row=2)
+    Label(app_add_project, text="task").grid(row=3)
+
+    project_name = Entry(app_add_project)
+    project_description = Entry(app_add_project)
+    project_start_dt = Entry(app_add_project)
+    project_task = Entry(app_add_project)
+
+    project_name.grid(row=0, column=1)
+    project_description.grid(row=1, column=1)
+    project_start_dt.grid(row=2, column=1)
+    project_task.grid(row=3, column=1)
+
+    add_button = Button(app_add_project, text='Add',
+                        command=lambda: submit_add_project(app_add_project, project_name.get(), project_description.get(), project_start_dt.get(), project_task.get()))
+    cancel_button = Button(app_add_project, text='Cancel',
+                           command=lambda: cancel_button_project(app_add_project))
+
+    add_button.grid(row=4, column=1)
+    cancel_button.grid(row=4, column=2)
+
+    app_add_project.geometry("500x300")
+    app_add_project.mainloop()
+
+
+def edit_project_window():
+    app_edit_project = Tk()
+    app_edit_project.title("Edit Project")
+
+    # Label(app_add_project, text="Add New Project Here", font=20).pack()
+
+    Label(app_edit_project, text="Project Name").grid(row=0)
+    Label(app_edit_project, text="Project Description").grid(row=1)
+    Label(app_edit_project, text="Start Date").grid(row=2)
+    Label(app_edit_project, text="task").grid(row=3)
+
+    project_name = Entry(app_edit_project)
+    project_description = Entry(app_edit_project)
+    project_start_dt = Entry(app_edit_project)
+    project_task = Entry(app_edit_project)
+
+    project_name.grid(row=0, column=1)
+    project_description.grid(row=1, column=1)
+    project_start_dt.grid(row=2, column=1)
+    project_task.grid(row=3, column=1)
+
+    add_button = Button(app_edit_project, text='Edit',
+                        command=lambda: submit_add_project(app_edit_project, project_name.get(), project_description.get(), project_start_dt.get(), project_task.get()))
+    cancel_button = Button(app_edit_project, text='Cancel',
+                        command=lambda: cancel_button_project(app_edit_project))
+
+    add_button.grid(row=4, column=1)
+    cancel_button.grid(row=4, column=2)
+
+    app_edit_project.geometry("500x300")
+    app_edit_project.mainloop()
+
+
+def show(label, clicked):
+    label.config(text=clicked.get())
+
+def delete_project_window():
+    app_edit_project = Tk()
+    app_edit_project.title("Edit Project")
+
+    # Label(app_add_project, text="Add New Project Here", font=20).pack()
+
+    options = [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday"
+    ]
+
+    # datatype of menu text
+    clicked = StringVar()
+
+    # initial menu text
+    clicked.set("Monday")
+
+    # Create Dropdown menu
+    drop = OptionMenu(app_edit_project, clicked, *options)
+    drop.pack()
+
+    # Create button, it will change label text
+    button = Button(app_edit_project, text="click Me",
+                    command=lambda: show(label, clicked)).pack()
+
+    # Create Label
+    label = Label(app_edit_project, text=" ")
+    label.pack()
+
+    app_edit_project.geometry("500x300")
+    app_edit_project.mainloop()
